9c6149bae6a2b360d1b207026943fac178db3575
sushma-Badam <sushmabadam29@gmail.com>
2019-03-04 14:17:15 +0530

    threads revised

--- windows/Program9/Program9/Source.cpp
+++ windows/Program9/Program9/Source.cpp
@@ -6,7 +6,7 @@ DWORD WINAPI thread_func(LPVOID lparam)
 {	
 	for (int i = 0; i < 10; i++) {
 		printf("\nprocess %d", i);
-		Sleep(1000);
+		Sleep(100);
 	}
 	return 0;
 }
@@ -14,13 +14,15 @@ int main()
 {
 	DWORD thid;
 	HANDLE hthread;
-	hthread = CreateThread(NULL, 0, thread_func, NULL, 0, &thid);
+	hthread = CreateThread(NULL, 0, thread_func, NULL, CREATE_SUSPENDED, &thid);
 	if (hthread == NULL)
 	{
 		printf("Unable to thread to create %d", GetLastError());
 		getchar();
 		return FALSE;
 	}
+	Sleep(5000);
+	ResumeThread(hthread);
 	CloseHandle(hthread);
 	printf("Waiting for user input to exit\n");
 	getchar();
